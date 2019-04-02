e676c0ee6e88fd2e1e95e9e79ce5900466a97541
trunecek.jakub@gmail.com <trunecek.jakub@gmail.com>
2019-04-02 14:21:12 +0200

    Some controller learning

--- Vidly/App_Start/RouteConfig.cs
+++ Vidly/App_Start/RouteConfig.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,11 +13,19 @@ namespace Vidly
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapMvcAttributeRoutes();
 
-            routes.MapRoute(
+            //routes.MapRoute(
+            //   "Test",
+            //   "Iceland/{Name}/{Slide}",
+            //   new {controller = "Catalog", action = "Test", Slide = UrlParameter.Optional },
+            //   new { Slide = @"\d{2}"}
+            //   );
+
+         routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Get", id = UrlParameter.Optional }
             );
         }
     }
---
+++ Vidly/Controllers/CatalogController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+
+namespace Vidly.Controllers
+{
+   public class CatalogController : Controller
+   {
+      public ViewResult Random()
+      {
+         var catalog = new Catalog()
+         {
+            Name = "WestFjords",
+            Id = 1
+         };
+
+         ViewData["WestFjords"] = catalog;
+
+         return View();
+      }
+
+      public ActionResult Edit(int id)
+      {
+
+         return Content("id:" + id);
+      }
+      [Route("Iceland/{Name}/{Slide:regex(\\d{2}):range(1,55)}")]
+      public ActionResult ShowSlide(string name, int? slide)
+      {
+         if (name == null)
+            name = "Name Was Null";
+         return Content(name + " slide: " + slide);
+      }
+
+
+      public ActionResult Slide(string presentationName, int? slide)
+      {
+         if (!slide.HasValue)
+            slide = 1;
+         return Content("ICeland in catalog: " + presentationName + "on slide " + slide);
+      }
+   }
+}
